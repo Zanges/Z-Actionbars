ff4f10a3e9b69f6817a9e2aa0294dfc5d98617ff
Dominik Peukert <zanges93@gmail.com>
2019-12-29 21:36:28 +0100

    Initial commit

---
+++ Locales/deDE.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Z-Actionbars", "deDE", false)
+
+if L then
+
+end
---
+++ Main.lua
@@ -0,0 +1,15 @@
+ZActionbars = LibStub("AceAddon-3.0"):NewAddon("Z-Actionbars", "AceConsole-3.0")
+local L = LibStub("AceLocale-3.0"):GetLocale("Z-Actionbars", true)
+
+
+function ZActionbars:OnInitialize()
+    -- Code that you want to run when the addon is first loaded goes here.
+end
+
+function ZActionbars:OnEnable()
+    -- Called when the addon is enabled
+end
+
+function ZActionbars:OnDisable()
+    -- Called when the addon is disabled
+end
